eba9a3f0e5a08f8e90bd45a4ffe44e9d79876d48
samlentz <66572076+samlentz@users.noreply.github.com>
2023-04-08 13:30:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-document.getElementById("submitButton").addEventListener("click", async () => {
-    const inputText = document.getElementById("inputText").value;
-    const apiUrl = "https://us-central1-famous-sunbeam-382202.cloudfunctions.net/function-3";
+const submitButton = document.getElementById("submitButton");
+const inputText = document.getElementById("inputText");
+const apiUrl = "https://us-central1-famous-sunbeam-382202.cloudfunctions.net/function-3";
+const loadingSpinner = document.getElementById("loadingSpinner");
+let audioContext;
 
-    if (inputText) {
+submitButton.addEventListener("click", async () => {
+    const inputValue = inputText.value;
+
+    if (inputValue) {
         try {
+            if (!audioContext) {
+                audioContext = new (window.AudioContext || window.webkitAudioContext)();
+            }
+
+            loadingSpinner.style.display = "block"; // Show loading spinner
+
             const response = await fetch(apiUrl, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
                 },
-                body: JSON.stringify({ message: inputText })
+                body: JSON.stringify({ message: inputValue })
             });
 
             if (response.ok) {
                 const arrayBuffer = await response.arrayBuffer();
-                const audioContext = new AudioContext();
                 const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
                 const source = audioContext.createBufferSource();
                 source.buffer = audioBuffer;
@@ -25,6 +35,8 @@ document.getElementById("submitButton").addEventListener("click", async () => {
             }
         } catch (error) {
             console.error("Error:", error);
+        } finally {
+            loadingSpinner.style.display = "none"; // Hide loading spinner
         }
     } else {
         alert("Please enter some text.");
