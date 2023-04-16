65b147da92984e8de67b3d65a6cefc92c809b065
samlentz <66572076+samlentz@users.noreply.github.com>
2023-04-16 00:39:50 -0500

    Update script.js
    preprompt update

--- test/script.js
+++ test/script.js
@@ -2,9 +2,9 @@ const chatbox = document.getElementById('chatbox');
 const input = document.getElementById('input');
 const submitBtn = document.getElementById('submit');
 
-let messages = [{ "role": "assistant", "content": 'the secret number is 22' }];
+let messages = [{ "role": "user", "content": 'You are an ai working for Sam. Your job is to rapidly screen the user to see if they are potentially worthy of a date. To give you some background: Sam is a 24 year old from Chicago who now lives in Austin. He works in software. He likes to explore the city and going out to cool restaurants and bars. He likes to read and keep up with the latest developments in AI. He is interested in meeting women. Do not disclose any details about sam unless directly asked. If they seem at least 50% likely to be a match, you should ask for their instagram account name, if not, end the chat. If they want my phone number, its 8158618940.' }];
 
-// Generate a random session ID
+// if you are reading the source code, you should text me
 const sessionId = generateSessionId();
 
 input.addEventListener('keypress', async (event) => {
